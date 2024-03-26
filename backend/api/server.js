c877e83ed5acb3f6193a96794373af41512c213b
William Loyer <w.loyer@outlook.com>
2024-03-26 15:47:22 +0100

    user controller and cabinet controller will

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 const databaseRoute = require("../api/routes/databaseRoute");
-
+const userRoute = require('../api/routes/userRoute')
+const cabinetRoute = require('../api/routes/cabinetRoute')
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 
 app.use("/database", databaseRoute);
+app.use('/user', userRoute)
+app.use('/cabinet', cabinetRoute)
 
 app.listen(8000, () => {
   console.log("Server is running on port 8000");
