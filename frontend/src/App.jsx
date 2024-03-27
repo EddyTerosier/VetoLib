3a4aa95c17d4908f9295571947976ecb781e6645
Samuel Tomen <113606314+samueltomen@users.noreply.github.com>
2024-03-27 12:29:35 +0100

    rattrapage betise  Carol

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Link, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
 import Home from "./components/Home.jsx";
 import Registration from "./components/Registration.jsx";
@@ -9,41 +9,6 @@ import "./App.css";
 function App() {
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid bg-dark my-0">
-          <Link to="/">Accueil</Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNavAltMarkup"
-            aria-controls="navbarNavAltMarkup"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div className="navbar-nav">
-              <li className="nav-link">
-                <Link to="/appointment">Prendre un RDV</Link>
-              </li>
-              <li className="nav-link">
-                <Link to="/profile">Mon profil</Link>
-              </li>
-              <li className="nav-link">
-                <Link to="/login">Login</Link>
-              </li>
-              <li className="nav-link">
-                <Link to="/registration">Registration</Link>
-              </li>
-              <li className="nav-link">
-                <Link to="/logout">Logout</Link>
-              </li>
-            </div>
-          </div>
-        </div>
-      </nav>
       <Routes>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/registration" element={<Registration />}></Route>
