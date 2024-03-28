df82e7669c20f8aa5ad93db1967fd149dd2a5169
William Loyer <w.loyer@outlook.com>
2024-03-28 15:23:10 +0100

    fegdfzg

--- frontend/src/components/Logout.jsx
+++ frontend/src/components/Logout.jsx
@@ -1,13 +1,16 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Cookies from 'js-cookie';
 
 const Logout = () => {
   const navigate = useNavigate();
 
+
   useEffect(() => {
-    document.cookie = "jwt=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT";
-    navigate('/login'); 
-  }, [navigate]);
+  Cookies.remove("jwt", "/" );
+  navigate('/login');
+  window.location.reload();
+}, [navigate]);
 
   return null; 
 };
